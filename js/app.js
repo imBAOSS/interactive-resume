1ad43988d8e740836ae6f049a25a8a95f9cf8f1c
imbaoss <baole6989@gmail.com>
2017-04-02 15:28:28 -0700

    Get scrolling position

--- js/app.js
+++ js/app.js
@@ -2,13 +2,24 @@ $(document).ready(init)
 
 function init() {
 
-  function createScrollPath() {
-    $.fn.scrollPath('getPath')
-      .moveTo(500, 360, {name: 'title-page'})
-      .lineTo(500, 1080, {name: 'start'})
-      .lineTo(1500, 1080, {name: 'first'});
-  }
+  $.fn.scrollPath('getPath', {scrollSpeed: 10})
+    .moveTo(500, 360, {name: 'title-page'})
+    .lineTo(500, 1080, {name: 'start'})
+    .lineTo(1500, 1080, {name: 'first'});
 
   $(".background-image").scrollPath({ drawPath: true });
 
 };
+
+function getScrollPosition() {
+  return $(window).scrollTop().valueOf();
+}
+
+$(window).scroll(function(event) {
+  let scrollPos = getScrollPosition();
+  console.log(scrollPos);
+});
+
+function orientHarry() {
+  // get scroll DIRECTION
+}
