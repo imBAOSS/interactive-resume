456f2173e11614951e00f3d800062be58d1e72a6
imbaoss <baole6989@gmail.com>
2017-04-07 16:12:58 -0700

    Add animation triggers

--- js/bundle.js
+++ js/bundle.js
@@ -72,40 +72,51 @@
 
 $(document).ready(function() {
 
-  $(window).keydown((e) => {
-    switch (e.keyCode) {
-      case 40:
-        return processDownArrow();
-      case 38:
-        return processUpArrow();
-      default:
-
-    }
-  })
+  // $(window).keydown((e) => {
+  //   switch (e.keyCode) {
+  //     case 40:
+  //       return processDownArrow();
+  //     case 38:
+  //       return processUpArrow();
+  //     default:
+  //
+  //   }
+  // })
 
 })
+
+function moveLayers(e) {
+  switch (e.keyCode) {
+    case 40:
+      return processDownArrow();
+    case 38:
+      return processUpArrow();
+  }
+}
 
 function processDownArrow() {
   let $top = $('.content').position().top;
   let $left = $('.content').position().left;
-  console.log($top);
 
-  let height = $(window).height();
-  let width = $(window).width();
+  console.log($('.content').position());
 
+  let $height = $(window).height();
+  let $width = $(window).width();
 
-  if ($top <= -height && $left <= -width) {
-    $left -= width*0.05
+  if ($top <= -$height*0.95 && $left >= -$width*0.95*2) {
+    $left -= $width*0.05
     $('.content').css('left', `${$left}px`)
   } else {
-    $top -= height*0.05
+    $top -= $height*0.05
     $('.content').css('top', `${$top}px`)
   }
+  console.log($('.content').position());
+
 }
 
 function processUpArrow() {
-  let $top = $('.content').css('top');
-  let $left = $('.content').css('left');
+  let $top = $('.content').position().top;
+  let $left = $('.content').position().left;
 
   let height = $(window).height();
   let width = $(window).width();
@@ -118,6 +129,28 @@ function processUpArrow() {
     $('.content').css('top', `${$top}px`)
   }
 }
+
+function  triggerAnimations() {
+  let $left = $('.content').position().left;
+
+  if ($left <= -1070) {
+    $('.three').addClass('animated fadeOut').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+      $(this).removeClass('animated fadeOut');
+    })
+  }
+}
+
+$(window).keydown(function(e) {
+  moveLayers(e);
+  triggerAnimations();
+  // positionHarry();
+})
+
+$(window).resize(function() {
+  // centerContent();
+  // calculate windowHeight and windowWidth
+  // calculate distance needed to travel
+})
 
 
 /***/ })
