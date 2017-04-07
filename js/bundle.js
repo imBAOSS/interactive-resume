51d43ce22a05ebecb76c3041c97f134de5cd5d0c
imbaoss <baole6989@gmail.com>
2017-04-06 19:51:08 -0700

    Set background via CSS

--- js/bundle.js
+++ js/bundle.js
@@ -72,26 +72,7 @@
 
 $(document).ready(function() {
 
-  $.fn.scrollPath('getPath', {scrollSpeed: 20})
-    .moveTo(500, 360, {name: 'title-page'})
-    .lineTo(500, 1080, {name: 'start'})
-    .lineTo(1500, 1080, {name: 'first'});
-
-  $(".background-image").scrollPath({ drawPath: true });
-
-  $(window).on('scroll', function(event) {
-    debugger;
-    let scrollPosition = $(document).scrollTop().valueOf();
-    console.log(scrollPosition);
-  });
-
-  function getScrollPosition() {
-    return $(document).scrollTop().valueOf();
-  }
-
-  function orientHarry() {
-    // get scroll DIRECTION
-  }
+  
 
 })
 
