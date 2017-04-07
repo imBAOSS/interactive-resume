cbe358006cfcbacefb8060214ee39b54f72348c3
imbaoss <baole6989@gmail.com>
2017-04-06 22:23:59 -0700

    Add force scrolling

--- js/bundle.js
+++ js/bundle.js
@@ -72,9 +72,49 @@
 
 $(document).ready(function() {
 
-  
+  $(window).keydown((e) => {
+    switch (e.keyCode) {
+      case 40:
+        return processDownArrow();
+      case 38:
+        return processUpArrow();
+      default:
+
+    }
+  })
 
 })
+
+function processDownArrow() {
+  let $top = $('.background-image').css('top');
+  let $left = $('.background-image').css('left');
+  let top = Number($top.match(/\-?\d+/))
+  let left = Number($left.match(/\-?\d+/))
+
+  if (top < -2300 && left <= 0) {
+    left -= 100
+    $('.background-image').css('left', `${left}px`)
+    console.log(left);
+  } else {
+    top -= 100
+    $('.background-image').css('top', `${top}px`)
+  }
+}
+
+function processUpArrow() {
+  let $top = $('.background-image').css('top');
+  let $left = $('.background-image').css('left');
+  let top = Number($top.match(/\-?\d+/))
+  let left = Number($left.match(/\-?\d+/))
+
+  if (top < -2300 && left >= 0) {
+    left += 100
+    $('.background-image').css('left', `${left}px`)
+  } else {
+    top += 100
+    $('.background-image').css('top', `${top}px`)
+  }
+}
 
 
 /***/ })
